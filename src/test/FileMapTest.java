885e74de17b5a955bf2cccc39ede2d51868ee732
kim841 <ktrinh7120@gmail.com>
2022-12-04 00:00:01 -0500

    add maven + junit 5

---
+++ src/test/FileMapTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileMapTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void containsKey() {
+    }
+
+    @Test
+    void containsValue() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void putAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void keySet() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void entrySet() {
+    }
+}
