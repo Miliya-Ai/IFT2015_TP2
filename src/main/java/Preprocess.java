0e72960ed8949187770d998619224560a5bde254
kim841 <ktrinh7120@gmail.com>
2022-12-08 16:18:25 -0500

    add code de tpiste

--- src/main/java/Preprocess.java
+++ src/main/java/Preprocess.java
@@ -79,7 +79,40 @@ public class Preprocess {
         }
     }
 
+/* ---------------------- Code de pretraitement de  Faezeh Pouya Mehr ------------------------------------------------
+-------------------- https://studium.umontreal.ca/mod/forum/discuss.php?d=1241377 ------------------------------------
+for (File file : listOfFiles){
+   BufferedReader br = new BufferedReader(new FileReader(new File(dir+"/"+file.getName())));
+     StringBuffer word = new StringBuffer();
+     String line;
+     while((line = br.readLine()) != null){
+           String newline = line.replaceAll("[^’'a-zA-Z0-9]", " ");
+           String finalline = newline.replaceAll("\\s+", " ").trim();
+           // set up pipeline properties
+           Properties props = new Properties();
+           // set the list of annotators to run
+           props.setProperty("annotators", "tokenize,pos,lemma");
+           // set a property for an annotator, in this case the coref annotator is being set to use the neural algorithm
+           props.setProperty("coref.algorithm", "neural");
+           // build pipeline
+           StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+           // create a document object
+           CoreDocument document = new CoreDocument(finalline);
+           // annnotate the document
+           pipeline.annotate(document);
+           //System.out.println(document.tokens());
+           for (CoreLabel tok : document.tokens()) {
+                 String str = String.valueOf(tok.lemma());
+                 if (!(str.contains("'s") || str.contains("’s"))) {
+                          word.append(str).append(" ");
+                 }
+           }
+      }
 
+String str = String.valueOf(word);
+str = str.replaceAll("[^a-zA-Z0-9]", " ").replaceAll("\\s+", " ").trim();
+}
+ */
 
 
 
