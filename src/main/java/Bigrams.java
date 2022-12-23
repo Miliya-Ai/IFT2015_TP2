addc33c5d54b2dad07db6878f6fdf5ca3dbfb26f
kim841 <ktrinh7120@gmail.com>
2022-12-23 15:30:21 -0500

    add Query class

--- src/main/java/Bigrams.java
+++ src/main/java/Bigrams.java
@@ -1,2 +1,12 @@
 public class Bigrams {
+
+    //suggesting the next most probable word like an auto-complete function which is
+    //related to the concept named bi-grams in NLP;
+
+    //constructor
+    public Bigrams(){
+
+    }
+
+
 }
---
+++ src/main/java/Query.java
@@ -0,0 +1,17 @@
+public class Query {
+
+    public Query() {
+        init();
+    }
+
+
+    public void init(){
+        // lire le fichier query.txt
+
+        // quand voit
+        // the most probable bigram of -> instancier bigrams
+        // search -> instancier TFIDF
+
+    }
+    
+}
