0aff99da2060e5928923e361e9377094ebee65fd
kim841 <ktrinh7120@gmail.com>
2022-12-24 00:17:15 -0500

    Update Struct.java

--- src/main/java/Struct.java
+++ src/main/java/Struct.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 public class Struct {
     WordMap wordMap = new WordMap();
     String[] allWords = null;
-    FileMap fileMap = new FileMap();
+    ArrayList wordConsecutif = new ArrayList<>();
+    ArrayList position = new ArrayList<>();
 
 
     public Struct(){
@@ -20,26 +21,23 @@ public class Struct {
         for (String word: allWords)
             if (!(wordMap.containsKey(word))){
                 FileMap fileMap = new FileMap();
-                fileMap.put(word, position);
+                fileMap.put(file, position);
+                wordMap.put(word, fileMap);
                 position ++;
             } else {
-                wordMap.getEntry(word).getValue().put();
-
-
+                wordMap.getEntry(word).getValue().put(file, position);
+                position ++;
             }
-        /*
-        1. pour chaque mot
-        if new mot
-            add dans worldMap
-            FileMap: key = nom file , value = position
-            position ++
-         else mot deja vu
-            if FileMap.key = currentFile
-                value.add(position)
-            else FileMap.key =/= currentFile
-                add FileMap, key , value
-         */
+
+        //TFIDF
+
+        //Bigrams
+        
+
     }
+
+
+
     public WordMap getWordMap(){
         return this.wordMap;
     }
