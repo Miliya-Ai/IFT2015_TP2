711a672fcdf01923fefc8022865798c6b298e799
Miliya-Ai <102935805+Miliya-Ai@users.noreply.github.com>
2022-12-23 21:04:56 -0500

    tfidf update

--- src/main/java/TFIDF.java
+++ src/main/java/TFIDF.java
@@ -1,2 +1,60 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.TreeMap;
+/**
+ * fonction : Chaîne de chemin entrant, retourne le texte sous la forme d’une chaîne
+ * Code inspire https://blog.csdn.net/yeyu_xing/article/details/109084754
+ */
 public class TFIDF {
-}
+    public static String getTxtString(String path) {
+        StringBuilder sBuilder = new StringBuilder();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(path));
+            char[] buffer = new char[512];
+            int len;
+            while ((len = br.read(buffer)) != -1) {
+                sBuilder.append(new String(buffer, 0, len));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) br.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sBuilder.toString();
+    }
+
+    /**
+     *
+     * Fonction: chaîne de texte entrant, fréquence de retour du mot
+     * @param txtString
+     * @return
+     */
+    public static Map<String, Integer> getCounter(String txtString) {
+        Map<String, Integer> treeMap = new TreeMap<>();
+        String[] strArr = txtString.split("[\\s?',!;.\u3000]+");
+        for (String s : strArr) {
+            treeMap.put(s, treeMap.getOrDefault(s, 0) + 1);
+        }
+        return treeMap;
+    }
+
+
+    public static void main(String[] args) {
+            String txtString = TFIDF.getTxtString("/Users/miliya.ai/Documents/GitHub/ift2015_TP2/dataset/dataset/901.txt");
+            Map<String, Integer> treeMap = TFIDF.getCounter(txtString);
+            treeMap.forEach((key, value) -> System.out.println(key + ": " + value));
+            treeMap.for(key){
+                i = value;
+                value = tem + value;
+        }
+        }
+    }
+
+
+
