d846f8c835da341c3b4a0c09646df9f57433c6bc
kim841 <ktrinh7120@gmail.com>
2022-12-23 21:00:47 -0500

    add comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         //double roundOff = Math.round(((double)8/11) * 100.0) / 100.0;
         //System.out.println(roundOff);
+        // pour windows "C:\\Users\\Admin\\Documents\\GitHub\\ift2015_TP2\\dataset\\dataset"
         new Preprocess("C:\\Users\\Admin\\Documents\\GitHub\\ift2015_TP2\\dataset\\dataset");
         //Struct struct = new Struct(path);
         //new Query(struct);
